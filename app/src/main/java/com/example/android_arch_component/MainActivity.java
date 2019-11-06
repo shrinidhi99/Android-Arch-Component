779cefa3d9ea6cb58c26c5be125d7871315a6f4e
shrinidhi99 <shrinidhi99.varna@gmail.com>
2019-11-06 22:48:46 +0530

    recycler view adapter

--- app/src/main/java/com/example/android_arch_component/MainActivity.java
+++ app/src/main/java/com/example/android_arch_component/MainActivity.java
@@ -6,7 +6,10 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.android_arch_component.Adapter.NoteAdapter;
 import com.example.android_arch_component.ViewModel.NoteViewModel;
 
 import java.util.List;
@@ -19,12 +22,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        final NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
         noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
                 // update recycler view
-                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+                adapter.setNotes(notes);
             }
         });
     }
